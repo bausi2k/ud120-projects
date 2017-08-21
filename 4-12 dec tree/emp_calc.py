2c73fbc5b0a0ee09e8d410acf97ea37557d5b4c6
Klaus Gruber <klausgruber@icloud.com>
2017-08-21 21:01:51 +0200

    Lesson 4 completed

---
+++ 4-12 dec tree/emp_calc.py
@@ -0,0 +1,8 @@
+import math
+
+a = 1
+b = 2.0 / 3
+
+print(a,b)
+print((a)*math.log((a), 2)-(b)*math.log((b), 2))
+print((a)*math.log((a), 2))
--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,9 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+import numpy as np
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -25,7 +27,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+def submitAccuracies():
+  return {"acc_min_samples_split":round(acc_min_samples_split_2,3)}
 
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+t0 = time()
+print("start time:", round(time()-t0, 3), "s")
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+
+t2 = round(time()-t0, 3)
+print ("training time:", t2, "s")
+
+labels_pred = clf.predict(features_test)
+
+
+t3 = round(time()-t2-t0, 3)
+print ("prediction time:", t3, "s")
+
+acc_min_samples_split_2 = accuracy_score(labels_test, labels_pred)
+
+
+print(submitAccuracies())
+
+#print(features_train.shape) # number of items and array headers
+#print(len(features_train)) # -> Number of Items
 #########################################################
 
 

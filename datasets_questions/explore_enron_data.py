b257963b8430e0e1ef866391f783731f6524eacb
bausi2k <bausi2k@gmail.com>
2017-08-31 22:13:22 +0200

    Up to Lesson 6-18 completed

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,33 @@
 
 import pickle
 
+j = 0
+
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_names = open("../final_project/poi_names.txt", "r")
+for person_name in enron_data:
+    #print(person_name)
+    if enron_data[person_name]["poi"] == 1:
+        j = j + 1
+        #print(enron_data[person_name]["poi"])
+
+#print("Daten: ", j)
 
 
+pois = 0
+for names in enron_names:
+    if names.find("("):
+        print("no poi ")
+    else:
+        print(names)
+        pois = pois + 1
+print("Number of Pois: ", pois)
+
+for person_name in enron_data:
+    #print(person_name)
+    if "prentice james" in person_name.lower():
+        #print("Not James")
+        #print(person_name)
+        for key in enron_data[person_name]:
+            if "stock" in key.lower():
+                print(key, enron_data[person_name][key])

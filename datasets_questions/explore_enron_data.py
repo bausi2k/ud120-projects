82b15cf9ebc20bc2921c9d9c84e5cf00acfea53f
bausi2k <bausi2k@gmail.com>
2017-09-02 13:38:31 +0200

    Lesson 6 completed.

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -39,6 +39,7 @@ for names in enron_names:
         pois = pois + 1
 print("Number of Pois:  ", pois)
 
+#prentice james 6-18
 for person_name in enron_data:
     #print(person_name)
     if "prentice james" in person_name.lower():
@@ -47,6 +48,7 @@ for person_name in enron_data:
         for key in enron_data[person_name]:
             if "stock" in key.lower():
                 print(key, enron_data[person_name][key])
+print("#### END Lesson 6-18")
 
 
 #Wesley Colwell 6-19
@@ -58,7 +60,7 @@ for person_name in enron_data:
          for key in enron_data[person_name]:
              if "from_this_person_to_poi" in key.lower():
               print(key, enron_data[person_name][key])
-              print("#### END Lesson 6-19")
+print("#### END Lesson 6-19")
 
 
 
@@ -72,11 +74,59 @@ for person_name in enron_data:
          for key in enron_data[person_name]:
              if "stock" in key.lower():
               print(key, enron_data[person_name][key])
-              print("#### END Lesson 6-20")
+print("#### END Lesson 6-20")
 
+#total_payments 6-25
+for person_name in enron_data:
+    #print(person_name)
+    if "skilling" in person_name.lower():
+        print("Person: {}".format(person_name))
+        print("Total Payment: {}".format(enron_data[person_name]["total_payments"]))
+    elif "lay" in person_name.lower():
+        print("Person: {}".format(person_name))
+        print("Total Payment: {}".format(enron_data[person_name]["total_payments"]))
+    elif "fastow" in person_name.lower():
+        print("Person: {}".format(person_name))
+        print("Total Payment: {}".format(enron_data[person_name]["total_payments"]))
+print("#### END Lesson 6-25")
 
+#No. email and salary values 6-27
+salc = 0
+emailc = 0
+for person_name in enron_data:
+    #print(person_name)
+    if enron_data[person_name]["salary"] != "NaN":
+        salc = salc + 1
+        #print("Name: {} with salary: {}".format(person_name,enron_data[person_name]["salary"]))
+    if enron_data[person_name]["email_address"] != "NaN":
+        #print("Name: {} with email: {}".format(person_name, enron_data[person_name]["email_address"]))
+        emailc = emailc + 1
 
+print("Valid salaries : {}; valid emails: {}".format(salc, emailc))
+print("#### END Lesson 6-27")
 
+#total_payments 6-29
+tpayc = 0
+count = 0
+for person_name in enron_data:
+    #print(person_name)
+    if enron_data[person_name]["total_payments"] == "NaN":
+        tpayc = tpayc + 1
+        print("Name: {} with total_payments: {}".format(person_name,enron_data[person_name]["salary"]))
+    else:
+        count = count + 1
+print("Number of persons without payment information: {} this is {}% of all {} persons".format(tpayc, round((1.0/(tpayc+count)*tpayc*100),2),(tpayc+count)))
+print("#### END Lesson 6-29")
 
-
-
+#total_payments@Pois 6-30
+tpayc = 0
+count = 0
+for person_name in enron_data:
+    if enron_data[person_name]["poi"]:
+        if enron_data[person_name]["total_payments"] == "NaN":
+            tpayc = tpayc + 1
+            print("Name: {} with total_payments: {}".format(person_name,enron_data[person_name]["salary"]))
+        else:
+            count = count + 1
+print("Number of poi persons without payment information: {} this is {}% of all {} persons".format(tpayc, round((1.0/(tpayc+count)*tpayc*100),2),(tpayc+count)))
+print("#### END Lesson 6-23")

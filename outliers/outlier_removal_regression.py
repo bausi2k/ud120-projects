bcd0d5e2a341037cbdcfd4bced2dd145fdba564d
bausi2k <bausi2k@gmail.com>
2017-09-07 05:56:29 +0000

    Up to Lesson 8-11

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,14 +25,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+#print(reg.predict(ages_train))
 
 
-
-
-
-
-
-
+print("coefficency: ",reg.coef_)
+print("Score: ", reg.score(ages_test, net_worths_test))
 
 
 
@@ -77,6 +77,7 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
+    plt.savefig('foo.png')
 
 
 else:

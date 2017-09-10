3a2289d030dbb011f7dafe392e19e8c0b622eaf8
bausi2k <bausi2k@gmail.com>
2017-09-10 21:22:24 +0200

    Lesson 8-12 completed

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -15,6 +15,24 @@ def outlierCleaner(predictions, ages, net_worths):
 
     ### your code goes here
 
-    
-    return cleaned_data
+    #got the resolution from: https://discussions.udacity.com/t/quiz-slope-after-cleaning/352475/2
+    ### this is where we modify our data
+    for pred, a, net_w in zip(predictions, ages, net_worths):
+        cleaned_data.append((a, net_w, pred - net_w))
 
+    #print(cleaned_data)
+    cleaned_data.sort(key=lambda i: i[2])  # Sort the data by the 2nd index (third element, which is the error)
+
+
+    '''
+    for i in range(len(ages)):
+        temp_data = (int(ages[i]), int(net_worths[i]), int((predictions[i] - net_worths[i])))
+        print(temp_data)
+        cleaned_data.append(temp_data)
+
+    for x in cleaned_data:
+        for y in x:
+            print(y)
+    '''
+
+    return cleaned_data[:81]  # returns the first 81 elements (or 90% of the original length)

509df328ad513c80940b138d80a3e0904cf874c3
bausi2k <bausi2k@gmail.com>
2017-09-11 21:46:44 +0200

    Lesson 8 ongoing

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -5,7 +5,7 @@ import sys
 import matplotlib.pyplot
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
+import numpy as np
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
@@ -14,6 +14,20 @@ data = featureFormat(data_dict, features)
 
 
 ### your code below
+max = (0,0)
+max = np.amax(data, axis=0)
+print(max[1])
+
+for i in data_dict:
+    if data_dict[i]["salary"] == max[0]:
+        print("Biggest Bonus winner: {}".format(i))
 
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+#matplotlib.pyplot.show()

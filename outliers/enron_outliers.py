2f1b509fe50f4c70973c6efb109d7edaa712d792
bausi2k <bausi2k@gmail.com>
2017-09-12 05:24:30 +0000

    Beautify Lesson 8-16

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -16,12 +16,12 @@ data = featureFormat(data_dict, features)
 ### your code below
 max = (0,0)
 max = np.amax(data, axis=0)
-print(max[1])
+print("Max element: ", max[0], max[1])
 
 for i in data_dict:
     if data_dict[i]["salary"] == max[0]:
         print("Biggest Bonus winner: {}".format(i))
-
+        print("These are the values of the Bonus Winner: ", data_dict[i])
 
 for point in data:
     salary = point[0]

9a627f5ab0e41849ecb0eba27001a323c59cf9c9
bausi2k <bausi2k@gmail.com>
2017-10-09 21:39:08 +0200

    Lesson 15 finished, get ready for the final exam!

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -29,3 +29,31 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+F_train, F_test, L_train, L_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(F_train, L_train)
+
+print(clf.score(F_test, L_test))
+
+print("POIs predicted: {}".format(clf.predict(F_test))) #15-29
+print("Test Labels: {}".format(L_test))                 #15-31
+print("POIs in total: {}".format(len(F_test)))          #15-30
+
+count = 0
+truecounter = 0
+for i in clf.predict(F_test):
+    if i == 1 and L_test[count] == 1:
+        truecounter += 1
+    count += 1
+
+print("How many predictions equals test_labels? {}".format(truecounter))    #15-31
+
+print("Precision score: {}".format(precision_score(L_test, clf.predict(F_test))))
+print("Recall score: {}".format(recall_score(L_test, clf.predict(F_test))))
+

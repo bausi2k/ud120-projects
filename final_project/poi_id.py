5e7daa5736d8e2962fabebeb7eb70972621b72c9
bausi2k <bausi2k@gmail.com>
2017-10-22 21:05:47 +0200

    started final project

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -6,15 +6,22 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from pprint import pprint
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 'to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
+
+print(len(data_dict))
+pprint(data_dict)
+# --> data_dict contians NaN values
+#TODO Think about changing the data...
+
 
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
@@ -43,8 +50,8 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test =  \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
